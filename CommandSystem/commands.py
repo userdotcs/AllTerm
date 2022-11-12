027712883094542f1ef4928e9d7d5df7f357af23
userdotcs <88928414+userdotcs@users.noreply.github.com>
2022-11-12 17:22:47 +0300

    Update commands.py

--- CommandSystem/commands.py
+++ CommandSystem/commands.py
@@ -17,3 +17,17 @@ class ExitCom(Command):
 
     def run(self):
         exit()
+
+
+class MkdCom(Command):
+    def __init__(self):
+        self.arg = "mkd"
+
+    def run(self):
+        pathlist = p.listpath(ti.cur_dir)
+        a = 1
+        for path in pathlist:
+            print(f"{a}) {path}")
+            a += 1
+
+        ti.cur_dir += f"/{pathlist[ti.get_int_terminal_input('Numb') - 1]}"
